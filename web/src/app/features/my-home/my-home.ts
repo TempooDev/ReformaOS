aeb9e5fd056a0d804c49a10821ab7a7df058859c
TempooDev <a.bermudez.freelance@gmail.com>
2026-03-30 20:16:04 +0200

    feat: add my-home

---
+++ web/src/app/features/my-home/my-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-my-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './my-home.html',
+  styleUrl: './my-home.css'
+})
+export class MyHomeComponent {}
